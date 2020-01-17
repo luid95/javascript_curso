dd38eaac4e5037d90ecd20ca751c8f62d5d83d70
Luis David Morales <luisdavidmorales95@gmail.com>
2020-01-17 14:00:26 -0600

    var, let const parte 2

--- var-let-const.js
+++ var-let-const.js
@@ -1,10 +1,42 @@
 alert("Funcionando");
 
+let i = 50;
+
 console.log("Var, let y const");
 
-for (var i = 0; i <= 5; i++) {
+console.log('Dentro del cilo for');
+
+for (let i = 0; i <= 5; i++) {
     console.log(i);
-    
 }
 
+console.log("Fuera del ciclo for");
+
 console.log(i);
+
+//Concicional if
+
+console.log("Condicional if");
+let num = 60;
+
+if(true){
+    let num = 5;
+    console.log("Dentro del if el valor es: " + num);    
+}
+console.log("Fuera de la condicional if es: " + num);
+
+///////////////
+let num1;//variable indefinida
+var num2;//variable indefinida
+
+const num3=44; //error, debe estar inicializado
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+// pero si podemos agregar valores, en cadenas
+const numeros = [10, 20, 30];
+console.log(numeros);
+ 
+numeros.push(40);
+console.log(numeros);
